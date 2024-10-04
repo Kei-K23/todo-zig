ac2313ea4480223782affcb6c546a8f40bf6e685
Kei-K23 <keiksl2301@gmail.com>
2024-10-04 21:01:48 +0630

    Add listTasks and makeAsComplete functions for tasks

--- src/main.zig
+++ src/main.zig
@@ -16,6 +16,12 @@ pub fn main() !void {
     const stdOut = std.io.getStdOut().writer();
 
     try stdOut.print("Todo List CLI - Zig (Version : {s})\n", .{version});
+    try addTask("To study");
+    try addTask("To Dance");
+
+    listTasks();
+    try makeAsComplete(1);
+    listTasks();
 }
 
 fn addTask(title: []const u8) !void {
@@ -25,8 +31,22 @@ fn addTask(title: []const u8) !void {
     try tasks.append(new_task);
 }
 
-fn listTasks() !void {
+fn makeAsComplete(id: u32) !void {
+    for (tasks.items) |*task| {
+        if (task.id == id) {
+            task.completed = true;
+            return;
+        }
+    }
+    std.debug.print("Task with ID {d} not found", .{id});
+}
+
+fn listTasks() void {
+    if (tasks.items.len == 0) {
+        std.debug.print("No task exist yet!\n", .{});
+    }
+
     for (tasks.items) |task| {
-        std.debug.print("ID: {d}, Title: {s}, Completed: {s}\n", .{task.id}, .{task.title}, .{task.completed});
+        std.debug.print("ID: {d}, Title: {s}, Completed: {s}\n", .{ task.id, task.title, if (task.completed) "Yes" else "No" });
     }
 }
